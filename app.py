44f12634145446a007bbb59ddf11d4e1f88b20a4
Fedecarota00 <giuse.dandrea99@gmail.com>
2025-04-07 21:55:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import openai
 # === CONFIGURATION ===
 HUNTER_API_KEY = "f68566d43791af9b30911bc0fe8a65a89908d4fe"
 PUBLIC_DOMAINS = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com"]
-openai.api_key = "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"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 JOB_KEYWORDS = ["Chief Executive Officer", "CEO", "Chief Financial Officer", "CFO", "Chief Operating Officer", "COO",
     "Chief Investment Officer", "CIO", "Chief Risk Officer", "CRO", "Chief Compliance Officer", "CCO",
